173d8788ff47f57d8cf93c7baa5d47605b3067a4
rohitrahinj0 <129195853+rohitrahinj0@users.noreply.github.com>
2026-07-16 22:47:41 +0530

    Add files via upload

---
+++ C++_Programming/SingleLevePrivate.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+  private:
+    int i,j;
+
+    public:
+    Base()
+    {
+        cout << "Inside Base Constructor\n";
+        i = 0;
+        j = 0;
+    }
+    ~Base()
+    {
+      cout << "Inside Base Destructor\n";
+    }
+    void fun()
+    {
+        cout << "Inside Base Fun\n";
+    }
+};
+
+class Derived : public Base
+{
+public:
+    int X;
+
+Derived()
+    {
+      cout << "Inside Derived Constructor\n";
+      X = 0;
+    }
+~Derived()
+      {
+        cout << "Inside Derived Destructor\n";
+      }
+      void gun()
+      {
+        cout << "Inside Gun of Derived\n";
+      }
+    
+};
+
+int main()
+{
+  cout << "Inside Main\n";
+  
+  Derived dobj;
+
+  cout <<"Size of Base Class object is : "<< sizeof(Base)<<"\n";               // 8
+  cout <<"Size of Derived Class object is : "<< sizeof(Derived)<<"\n";         //12
+
+//cout <<dobj.i<<"\n";        //error
+//cout <<dobj.j<<"\n";        //error
+cout <<dobj.X<<"\n";        //0
+
+
+
+cout << "End of Main\n";
+  return 0;
+}
